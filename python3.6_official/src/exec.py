d4dfd5854d46aaebad26ec5a53b8eecbc583c327
a-kanda <tderkkwii@yahoo.co.jp>
2019-08-15 00:55:35 +0900

    スクレイプング環境を追加

---
+++ python3.6_official/src/exec.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+
+print("HELLO python3.6_official")
+print("./src/exec.pyに記述したプログラムが実行されます。")
---
+++ python_scrape/src/exec.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+
+
+print("HELLO python_scraping")
+print("./src/exec.pyに記述したプログラムが実行されます。")
+print("./pip/requirements.txtに記述したライブラリがインストールされています。")
