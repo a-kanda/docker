8bd704b12278bcc123dfbd5cae1a47879153ba3e
a-kanda <tderkkwii@yahoo.co.jp>
2019-11-02 23:57:44 +0900

    デフォルトの実行ファイルを変更

--- python3.6_official/src/exec.py
+++ python3.6_official/src/exec.py
@@ -1,4 +1,10 @@
 # coding:utf-8
+import subprocess
 
 print("HELLO python3.6_official")
 print("./src/exec.pyに記述したプログラムが実行されます。")
+
+exec_file = "/work/git/trade/exec.py"
+
+subprocess.call(['python3.6',exec_file])
+
