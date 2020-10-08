c8e78f5be72b50f61d8ebc9f215ea5d0494b6d8e
a-kanda <tderkkwii@yahoo.co.jp>
2020-10-09 03:01:32 +0900

    ディスクのマウントを追加

--- jupyter_deeplearning/run.sh
+++ jupyter_deeplearning/run.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -u $(id -u):$(id -g) -d -p 8888:8888 tensorflow/tensorflow:latest-gpu-py3-jupyter
+docker run --gpus all -u $(id -u):$(id -g) -d -p 8888:8888 -v /work:/tf/work  tensorflow/tensorflow:latest-gpu-py3-jupyter
